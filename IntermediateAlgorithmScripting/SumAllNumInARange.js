a91bb7b5a7a38da81d9a9461845c37c5cc101048
thong <duythong2707@gmail.com>
2017-11-29 19:41:59 +0700

    sd

---
+++ IntermediateAlgorithmScripting/SumAllNumInARange.js
@@ -0,0 +1,20 @@
+//We'll pass you an array of two numbers. Return the sum of those two
+// numbers and all numbers between them.
+//The lowest number will not always come first.
+
+function sumAll(arr) {
+  var min=Math.min(...arr)
+  var max=Math.max(...arr)
+
+  var tempArr=[]
+  for(var i=min; i<=max;i++){
+  	tempArr.push(i)
+  }
+  console.log(tempArr)
+  var result= tempArr.reduce((sum,current)=>
+  	sum+current)
+return result
+}
+
+//var a=sumAll([10, 4]);
+console.log(sumAll([10, 4]))
