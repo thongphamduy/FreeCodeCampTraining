4184b8a3ae55b5347593bd366aeaa3968948ad66
Thong <thong@inspectorio.com>
2017-11-30 22:06:15 +0700

    dS

--- IntermediateAlgorithmScripting/DiffTwoArrays.js
+++ IntermediateAlgorithmScripting/DiffTwoArrays.js
@@ -5,18 +5,7 @@
 
 function diffArray(arr1, arr2) {
   var newArr = [];
-  var newArr1=[]
-  var newArr2=[]
-
-  for(var i=0;i<arr1.length;i++){
-  	var i2=arr2.indexOf(arr1[i])
-  	if(i2 !== -1){
-  		newArr1.push(arr1.slice(i, i+1))
-  		newArr2.push(arr2.slice(i2, i2+1))
-  	}
-  }  
-  newArr=newArr2.concat(newArr1)
-  console.log(newArr)
+  
   return newArr;
 }
 
