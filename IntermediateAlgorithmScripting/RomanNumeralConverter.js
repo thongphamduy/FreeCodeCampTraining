08bb6872c53967ad87c20b0ece3ebbba77faeb7a
thong <duythong2707@gmail.com>
2017-12-01 20:17:56 +0700

    bai 3

---
+++ IntermediateAlgorithmScripting/RomanNumeralConverter.js
@@ -0,0 +1,27 @@
+//Convert the given number into a ronman numeral. 
+//All roman numberal answers should be provided in upper-case
+
+function convertToRoman(num) {
+	var arr=[]
+	var roman=[1000,500,100,50,10,5,1]
+	var key=["M","D","C","L","X","V","I"]
+	var arr1=[]
+	for(var i=0;i<roman.length;i++){
+		var thuong=Math.floor(num/roman[i])
+		arr.push(thuong)
+		num=num%roman[i]
+		console.log(num)
+	}
+	console.log(arr)
+	for(var i=0;i<arr.length;i++){
+		for(var j=0;j<arr[i];j++){
+			arr1.push(key[i])
+		}
+	}
+
+	console.log(arr1.join(""))
+
+	return num
+}
+
+convertToRoman(4);
